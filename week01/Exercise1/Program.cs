5998f9342598d58cbc416262e9d39a55c2109f58
Cayden Lunt <cayden.lunt@gmail.com>
2026-01-07 00:19:31 -0700

    completed program 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string fName = Console.ReadLine().Trim();
+        if (!string.IsNullOrEmpty(fName))
+            fName = char.ToUpper(fName[0]) + fName[1..];
+
+        Console.Write("What is your last name? ");
+        string lName = Console.ReadLine().Trim();
+        if (!string.IsNullOrEmpty(lName))
+            lName = char.ToUpper(lName[0]) + lName[1..];
+
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
     }
 }
