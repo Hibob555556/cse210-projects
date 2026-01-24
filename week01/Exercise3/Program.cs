17d14eb814201e5fdf9c1319c0c309f79e00ba81
Cayden Lunt <cayden.lunt@gmail.com>
2026-01-23 18:28:01 -0700

    progress: part 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool replay = true;
+        while (replay)
+        {
+            // generate random number
+            var rand = new Random();
+            int magicNumber = rand.Next(1, 101);
+
+            // run loop til guessed
+            bool playing = true;
+            int guesses = 0;
+            while (playing)
+            {
+                guesses++;
+                Console.Write("What your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+
+                string hint = String.Empty;
+                if (guess < magicNumber)
+                    hint = "Higher";
+                else if (guess > magicNumber)
+                    hint = "Lower";
+                else
+                {
+                    Console.WriteLine($"\nYou guessed it!\nIt took you {guesses} {(guesses == 1 ? "guess" : "guesses")}");
+                    Console.Write("Would you like to play again? (1 = yes, 0 = no) ");
+                    int choice = int.Parse(Console.ReadLine());
+                    if (choice != 1)
+                    {
+                        replay = false;
+                        break;
+                    }
+                    playing = false;
+                    break;
+                }
+
+                Console.WriteLine(hint);
+            }
+        }
+        Console.WriteLine("Goodbye!");
     }
 }
