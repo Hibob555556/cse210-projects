b82f01771fd0f1619e748acfe7d5c8eec9a7f77b
Cayden Lunt <cayden.lunt@gmail.com>
2026-01-23 18:53:04 -0700

    progress: exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // initialize empty list
+        List<int> nums = [];
+
+        // get list of nums from user
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int entry = 1;
+        while (entry != 0)
+        {
+            Console.Write("Enter number: ");
+            entry = int.Parse(Console.ReadLine());
+            if (entry != 0)
+                nums.Add(entry);
+        }
+
+        // calc sum
+        int sum = nums.Sum();
+        Console.WriteLine($"\nThe sum is: {sum}");
+
+        // calc average
+        try
+        {
+            double avg = nums.Average();
+            Console.WriteLine($"The average is: {avg}");
+        }
+        catch
+        {
+            Console.WriteLine("The average is: 0");
+        }
+
+        // calc max
+        try
+        {
+            int max = nums.Max();
+            Console.WriteLine($"The largest number is: {max}");
+        }
+        catch
+        {
+            Console.WriteLine("The largest number is: 0");
+        }
+
+        // calculate smallest number > 0
+        List<int> minList = nums.FindAll(n => n > 0);
+        int min = 0;
+        if (minList.Count > 0)
+        {
+            min = minList.Min();
+            Console.WriteLine($"The smallest positive number is: {min}");
+        }
+        else
+        {
+            Console.WriteLine("The smallest positive number is: (no positive numbers found)");
+        }
+
+        // sort and display list
+        Console.WriteLine("The sorted list is:");
+        nums.Sort();
+        nums.ForEach((n) =>
+        {
+            Console.WriteLine(n);
+        });
     }
 }
